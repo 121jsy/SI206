f3c45f13734a76b78342f6211309f08721efd9e7
121jsy <121jsy@gmail.com>
2025-04-04 18:12:53 -0400

    Corrected filenames

--- config.py
+++ config.py
@@ -5,23 +5,21 @@ from dotenv import load_dotenv
 # Load variables from .env into the environment
 load_dotenv()
 
-def get_api_key(api_name: str) -> str:
+def get_news_api_auth_header():
     """
     Fetch an API key from environment variables. Raises ValueError if the API key is not found
 
     Args:
-        api_name (str): variable name in .env file (e.g., 'NEWS_API_KEY')
+        None
 
     Returns:
         dict: authorization HTTP header
     """
-    key = os.getenv(api_name)
+    key = os.getenv("NEWS_API_KEY")
     if key is None:
-        raise ValueError(f"API key for '{api_name}' not found.")
-    return key
+        raise ValueError("API key for 'NEWS_API_KEY' not found.")
 
-def get_news_api_key():
-    return {"Authorization": get_api_key("NEWS_API_KEY")}
+    return {"Authorization": key}
 
-def get_pytrends_api_key() -> str:
-    return {"Authorization": get_api_key("PYTRENDS_API_KEY")}
+# def get_pytrends_api_key() -> str:
+#     return {"Authorization": get_api_key("PYTRENDS_API_KEY")}
